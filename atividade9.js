112e2895570f6b6435d28262f08565fb3cbc9ead
ramonsilv <ramonsilvaf@gmail.com>
2023-06-24 18:33:33 -0300

    atividades realizadas do 2 ate o 5
    Signed-off-by: ramonsilv <ramonsilvaf@gmail.com>

--- atividade9.js
+++ atividade9.js
@@ -40,14 +40,53 @@ let produto8 = new Produto("32BR95J8", "Calça", "45")
 
 // console.log(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8)//(EXERCICIO 2)
 
-let venda1 = new Venda("25BR25J1", "19/06/2023", "David Martins", "Tenis-azul")
-let venda2 = new Venda("26BR35J2", "20/06/2023", "Andre Valadao", "Mochila-Preta")
-let venda3 = new Venda("27BR45J3", "21/06/2023", "Gabriel Santos", "Boné-Cinza")
-let venda4 = new Venda("28BR55J4", "22/06/2023", "Samuel Backer", "Chaveiro-Prata")
-let venda5 = new Venda("30BR75J6", "23/06/2023", "Ilton Salazar", "Chinelo-Branco")
+let venda1 = new Venda("25BR25J1", "19/06/2023 08:00", "David Martins", "Tenis-azul")
+let venda2 = new Venda("26BR35J2", "20/06/2023 09:00", "Andre Valadao", "Mochila-Preta")
+let venda3 = new Venda("27BR45J3", "21/06/2023 10:00", "Gabriel Santos", "Boné-Cinza")
+let venda4 = new Venda("28BR55J4", "22/06/2023 11:00", "Samuel Backer", "Chaveiro-Prata")
+let venda5 = new Venda("30BR75J6", "23/06/2023 13:00", "Ilton Salazar", "Chinelo-Branco")
 
 // console.log(venda1, venda2, venda3, venda4, venda5)//(EXERCICIO 2)
 
-let cliente = []
-let produto = []
-let venda = []
+//-----------------------------------------------------------------------------------------------------
+
+let clientes = [cliente1, cliente2, cliente3];
+let produtos = [produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8];
+let vendas = [venda1, venda2, venda3, venda4, venda5];
+
+// console.log(clientes);//(EXERCICIO 3)
+clientes.forEach(nomes => {
+    // console.log("Nome: " + nomes)
+}) //(EXERCICIO 3)
+
+// console.log(produtos);//(EXERCICIO 3)
+produtos.forEach(produtos => {
+    // console.log("Descrição: " + produtos)
+}) //(EXERCICIO 3)
+
+// console.log(vendas);//(EXERCICIO 3)
+vendas.forEach(vendas =>{
+    // console.log("Data e Hora: " + vendas)
+}) //(EXERCICO 3)
+
+//---------------------------------------------------------------------------------------------------
+
+let clientesMap = new Map(clientes.map(nome => [nome.cpf, [nome.nome, nome.datanascimento]]));
+// console.log(clientesMap)     //(EXERCICIO 4)
+
+let produtosMap = new Map(produtos.map(prod => [prod.codigo, [prod.descricao, prod.qtdestoque]]));
+// console.log(produtosMap)    //(EXERCICIO 4)
+
+let vendasMap = new Map(vendas.map(vend => [vend.codigo, [vend.datahora, vend.cliente, vend.produto]]));
+// console.log(vendasMap)  //(EXERCICIO 4)
+
+//-----------------------------------------------------------------------------------------------------
+
+let clienteMap = new Map(clientes.map(nome => [nome.cpf, { nome: nome.nome, datanascimento: nome.datanascimento }]));
+// console.log(clienteMap) //(EXERCICIO 5)
+let produtoMap = new Map(produtos.map(prod => [prod.codigo, { descricao: prod.descricao, qtdestoque: prod.qtdestoque }]));
+// console.log(produtoMap) //(EXERCICIO 5)
+let vendaMap = new Map(vendas.map(vend => [vend.codigo, { datahora: vend.datahora, cliente: vend.cliente, produto: vend.produto }]));
+// console.log(vendaMap)   //(EXERCICIO 5)
+
+//-----------------------------------------------------------------------------------------------------
